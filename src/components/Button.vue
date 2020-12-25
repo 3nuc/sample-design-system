4505955bdaab08aa9708893ccbffa2fa9dc05b1c
Artur Tagisow <atagisov@protonmail.com>
2020-12-25 03:32:12 +0100

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+<button class="ds-button" @click="emit('click')">
+ <slot/>
+</button>
+</template>
+
+<script setup>
+/* eslint-disable */
+import {defineEmit} from 'vue'
+const emit = defineEmit(['click'])
+
+</script>
+
+<style>
+.ds-button {
+  background-color: red;
+}
+</style>
